beda54b110e21f58aaedf0f46fc558d74490e57f
unknown <josemauricio.gomes@gmail.com>
2021-11-07 00:17:44 -0300

    Organizando direotrios

---
+++ precos/src/main/java/com/mgtech/precos/precos/model/Preco.java
@@ -0,0 +1,15 @@
+package com.mgtech.precos.precos.model;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Preco {
+	
+	private Long codigoProduto;
+	private BigDecimal preco;
+
+}
